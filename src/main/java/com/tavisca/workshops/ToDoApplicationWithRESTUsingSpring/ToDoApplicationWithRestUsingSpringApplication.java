e7c22b5fd0c5674c88314a0cf0a2c05140356509
vnayal <vnayal@tavisca.in>
2019-09-09 11:02:48 +0530

    Initial Commit

---
+++ src/main/java/com/tavisca/workshops/ToDoApplicationWithRESTUsingSpring/ToDoApplicationWithRestUsingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tavisca.workshops.ToDoApplicationWithRESTUsingSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApplicationWithRestUsingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApplicationWithRestUsingSpringApplication.class, args);
+	}
+
+}
